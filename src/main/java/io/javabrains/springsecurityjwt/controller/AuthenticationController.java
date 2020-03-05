51ded11695bcb410d851f2409690f9edaaffacc7
Rafael Pacce Meireles <rafaelpaccemeireles@hotmail.com>
2020-03-05 15:18:10 -0300

    Separação classe application e controler

---
+++ src/main/java/io/javabrains/springsecurityjwt/controller/AuthenticationController.java
@@ -0,0 +1,127 @@
+package io.javabrains.springsecurityjwt.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.springframework.core.env.Environment;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.javabrains.springsecurityjwt.models.AuthenticationRequest;
+import io.javabrains.springsecurityjwt.util.JwtUtil;
+import kong.unirest.HttpResponse;
+import kong.unirest.Unirest;
+import okhttp3.OkHttpClient;
+
+@RequestMapping("/pocs/v1")
+@RestController
+public class AuthenticationController {
+
+    OkHttpClient client = new OkHttpClient();
+
+    @Autowired
+    private Environment env;
+
+    @Autowired
+    private JwtUtil jwtTokenUtil;
+
+    @RequestMapping(value = "/authenticate", produces = "application/json", method = RequestMethod.POST)
+    @ResponseBody
+    public ResponseEntity<JSONObject> createAuthenticationToken(
+            @RequestBody AuthenticationRequest authenticationRequest) throws Exception {
+
+        final String jwt = jwtTokenUtil.generateJWTAssertionFromString(authenticationRequest.getKeyPublic(),
+                authenticationRequest.getKeyPrivate(), authenticationRequest.getoAuthBasePath(),
+                authenticationRequest.getClientId(), authenticationRequest.getUserId());
+
+        String base_url_docusign = env.getProperty("url.dev.docusign");
+        String grant_type = env.getProperty("grant.docusign");
+
+        HttpResponse<String> response = Unirest.post(base_url_docusign + "/oauth/token")
+                .header("accept", "application/json").header("content-type", "application/json")
+                .queryString("assertion", jwt).queryString("grant_type", grant_type).asString();
+
+        JSONParser parser = new JSONParser();
+        JSONObject json = (JSONObject) parser.parse(response.getBody());
+
+        return ResponseEntity.ok(json);
+    }
+
+    //
+    // @RequestMapping(value = "/oauth/token", method = RequestMethod.POST)
+    // public ResponseEntity<?> getAccessToken(@RequestParam("grant_type") String
+    // grant_type
+    // ,@RequestParam("assertion") String assertion
+    // /*,@RequestParam("redirect_uri") String redirect_uri*/) throws Exception {
+    //
+    // //AuthenticationRequest authenticationRequest = new AuthenticationRequest();
+    //
+    // String base_url_docusign = env.getProperty("url.dev.docusign");
+    //
+    // HttpUrl.Builder urlBuilder = HttpUrl.parse(base_url_docusign +
+    // "/oauth/token").newBuilder();
+    // urlBuilder.addQueryParameter("grant_type", grant_type);
+    // urlBuilder.addQueryParameter("assertion", assertion);
+    // //urlBuilder.addQueryParameter("redirect_uri", redirect_uri);
+    //
+    // System.out.println(urlBuilder);
+    //
+    // String url = urlBuilder.build().toString();
+    //
+    // Request request = new Request.Builder().url(url)
+    // .addHeader("Content-Type", "application/json")
+    // .build();
+    //
+    // Response response = client.newCall(request).execute();
+    //
+    // System.out.println(response);
+    //
+    // return ResponseEntity.ok(response);
+    //
+    // //return ResponseEntity.ok(new AuthenticationResponse(jwt));
+    // }
+
+    //
+    // @RequestMapping(value = "/oauth/token", method = RequestMethod.POST)
+    // public JSONObject getAccessToken(@RequestParam("grant_type") String
+    // grant_type
+    // ,@RequestParam("assertion") String assertion
+    // /*,@RequestParam("redirect_uri") String redirect_uri*/) throws Exception {
+    //
+    // String base_url_docusign = env.getProperty("url.dev.docusign");
+    //
+    // HttpResponse<String> response = Unirest.post(base_url_docusign +
+    // "/oauth/token").
+    // header("accept", "application/json").
+    // header("content-type", "application/json").
+    // queryString("assertion", assertion).
+    // queryString("grant_type", grant_type).
+    // asString();
+    //
+    // System.out.println(response.getBody());
+    //
+    // JSONParser parser = new JSONParser();
+    // JSONObject json = (JSONObject) parser.parse(response.getBody());
+    //
+    // return json;
+    //
+    // //return ResponseEntity.ok(new AuthenticationResponse(jwt));
+    // }
+
+    //
+    // @RequestMapping(value = "/users", method = RequestMethod.GET)
+    // public String getUsersUsingUnirest() throws Exception {
+    // HttpResponse<String> response = Unirest.get("http://localhost:3000/users").
+    // header("Content-Type", "application/json").
+    // asString();
+    // System.out.println(response.getBody());
+    // //System.out.println(response.getBody().getObject().toString(2));
+    //
+    // return response.getBody();
+    // }
+
+}
